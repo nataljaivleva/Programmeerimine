dc837a83383ffd474210646cd0f26673b03f9490
natalja.ivleva <nativl@mail.com>
2018-12-17 22:12:29 +0200

    Added Example (Leeson 1: SumNumbers)

---
+++ 01_Lesson/SumNumbers.java
@@ -0,0 +1,9 @@
+public class SumNumbers {
+    public static void main(String[] args) {
+        System.out.println("======== Сложение чисел ===========");
+        int number1 = 10;
+        int number2 = 15;
+        int sum = number1 + number2;
+        System.out.println("Сумма двух " + number1 + " и " + number2 + " чисел равна: " + sum);
+    }
+}
